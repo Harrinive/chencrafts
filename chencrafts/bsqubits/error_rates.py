7c00007b85fcd0601b0c57bae96a99782e5dd561
Danyang Chen <dychen1999@foxmail.com>
2022-10-13 13:03:20 -0500

    Error rates

--- chencrafts/bsqubits/error_rates.py
+++ chencrafts/bsqubits/error_rates.py
@@ -1,3 +1,4 @@
+from collections import OrderedDict
 import numpy as np
 import scqubits as scq
 
@@ -6,33 +7,31 @@ from chencrafts.bsqubits import single_sweep_tmon, sweep_tmon, generate_variable
 
 from typing import Callable, List
 
-class error_channel:
+class ErrorChannel:
     def __init__(
         self, 
         name: str, 
         input_var_names: List[str], 
         expression: Callable,
-        input_mode: str = "original",
+        default_input_mode: str = "original",
         full_var_names: List[str] = None,
     ):
         self.name = name
         self.input_var_names = input_var_names
         self.expression = expression
 
-        if input_mode in ["original", "full", "dict"]:
-            self.input_mode = input_mode
+        if default_input_mode in ["original", "full", "dict"]:
+            self.default_input_mode = default_input_mode
         else:
-            raise ValueError(f"Input mode {input_mode} is invalid, only use "
+            raise ValueError(f"Input mode {default_input_mode} is invalid, only use "
             "\"original\", \"full\" and \"dict\".")
 
-        self.variable_full_idx = None
         if full_var_names is not None:
             self.variable_full_idx = (
-                [full_var_names.index(variable) for variable in self.input_var_name]
+                [full_var_names.index(variable) for variable in self.input_var_names]
             )
-        elif self.input_mode == "full":
-            raise ValueError(f"For using the full variable list as an input, "
-            "you should specify the full_var_names")
+        else:
+            self.variable_full_idx = None
 
     def __call__(self, args, input_mode=None):
         # try the assigned input_mode first
@@ -42,13 +41,24 @@ class error_channel:
             return self._er_from_full(args)
         elif input_mode == "dict":
             return self._er_from_dict(args)
+        elif input_mode is None:
+            if self.default_input_mode == "original":
+                return self._error_rate(args)
+            elif self.default_input_mode == "full":
+                return self._er_from_full(args)
+            elif self.default_input_mode == "dict":
+                return self._er_from_dict(args)
+        else:
+            raise ValueError(f"Input mode {input_mode} is invalid, should use "
+            "None, \"original\", \"full\" and \"dict\".")
 
-        if self.input_mode is None:
-            return self._error_rate(args)
-        elif self.input_mode == "full":
-            return self._er_from_full(args)
-        elif self.input_mode == "dict":
-            return self._er_from_dict(args)
+    def update_full_var_names(
+        self,
+        full_var_names: List[str],
+    ):
+        self.variable_full_idx = (
+            [full_var_names.index(variable) for variable in self.input_var_names]
+        )
 
     def _error_rate(self, args):
         # input should be an array/list
@@ -65,6 +75,7 @@ class error_channel:
             raise ValueError(f"For using the full variable list as an input, "
             "you should specify the full_var_names")
 
+        args = np.array(args)
         params = args[self.variable_full_idx]
         return self.expression(*params)
 
@@ -76,7 +87,95 @@ class error_channel:
         params = [arg_dict[arg] for arg in self.input_var_names]
         return self.expression(*params)
 
-class ErrorRateBase:
-    # error channels
-    # 
-    pass
+class ErrorRate:
+    def __init__(
+        self, 
+        full_var_names: List[str],
+        default_input_mode: str = "full",
+    ):
+        self.full_var_names = full_var_names
+
+        if default_input_mode in ["original", "full", "dict"]:
+            self.default_input_mode = default_input_mode
+        else:
+            raise ValueError(f"Input mode {default_input_mode} is invalid, only use "
+            "\"original\", \"full\" and \"dict\".")
+
+        self.error_channels = OrderedDict({})
+        self.channel_enable_info = OrderedDict({})
+
+    def __call__(
+        self,
+        args,
+        return_dict: bool = False,
+        input_mode: str = None,
+    ):
+        """returns the total enabled error rate"""
+        if return_dict:
+            error_dict = OrderedDict({})
+        else:
+            total_error = 0
+
+        for name, error_channel in self.error_channels.items():
+            error_rate = (
+                error_channel(args, input_mode=input_mode)
+                * self.channel_enable_info[name]
+            )
+            
+            if return_dict:
+                error_dict[name] = error_rate
+            else:
+                total_error += error_rate
+
+        if return_dict:
+            return error_dict
+        else:
+            return total_error
+
+    def __getitem__(
+        self,
+        error_name
+    ):
+        """calculate the error rate from a single channel"""
+        return self.error_channels[error_name]
+
+    def add_channel(
+        self,
+        name: str, 
+        input_var_names: List[str], 
+        expression: Callable,
+    ):
+        channel = ErrorChannel(
+            name,
+            input_var_names, 
+            expression,
+            self.default_input_mode,
+            self.full_var_names,
+        )
+        self.error_channels[name] = channel
+        self.channel_enable_info[name] = True
+
+    def add_existed_channel(
+        self,
+        channel: ErrorChannel,
+    ):  
+        name = channel.name
+
+        channel.default_input_mode = self.default_input_mode
+        channel.update_full_var_names(self.full_var_names)
+
+        self.error_channels[name] = channel
+        self.channel_enable_info[name] = True
+
+    def disable_channel(
+        self,
+        name: str
+    ):
+        self.channel_enable_info[name] = False
+
+    def enable_channel(
+        self,
+        name: str
+    ):
+        self.channel_enable_info[name] = True
+        
