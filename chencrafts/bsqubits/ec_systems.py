f7935d71fa7dad598ac69ab5351d556230439ba1
Danyang Chen <dychen1999@foxmail.com>
2023-01-09 16:29:11 -0600

    markout print

--- chencrafts/bsqubits/ec_systems.py
+++ chencrafts/bsqubits/ec_systems.py
@@ -416,9 +416,9 @@ class CavityFlxnSys(CavityAncSystem):
             else:
                 break
 
-        print(f"A flxn is successfully created!")
+        # print(f"A flxn is successfully created!")
         if update_cutoff:
-            print(f"Cutoff: {sim_para['anc_cutoff']} -> {anc_cutoff}")
+            # print(f"Cutoff: {sim_para['anc_cutoff']} -> {anc_cutoff}")
             sim_para["anc_cutoff"] = anc_cutoff
 
         self.subsys = [self.system, self.ancilla]
