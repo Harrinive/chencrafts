bbe27321752a95a7c9f471366880fef74d8f97ae
Danyang Chen <dychen1999@foxmail.com>
2023-06-07 15:21:06 -0500

    put spec fit into chencraft

---
+++ chencrafts/specfit/__init__.py
@@ -0,0 +1,11 @@
+from chencrafts.specfit.extract_peaks import (
+    plot_peaks,
+    get_peaks,
+    remove_peaks,
+)
+
+from chencrafts.specfit.fit import (
+    combine_peaks_dict,
+    process_selected_data,
+    MSE,
+)
