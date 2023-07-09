f1f854b70438cf79b032789e8de0798bcb52ae16
Danyang Chen <danyangchen2026@u.northwestern.edu>
2023-07-09 17:49:36 -0500

    test

--- tests/test_optimization.py
+++ tests/test_optimization.py
@@ -1,8 +1,11 @@
 import numpy as np
 import os
 from chencrafts.toolbox.optimize import Optimization, MultiOpt, OptTraj, MultiTraj
+import time
 
-def target_func(coord, offset=[0, 0, 0]):
+def target_func(coord, offset=[0, 0, 0], error_prob=0.0):
+    if np.random.rand() < error_prob:
+        raise ValueError("Random error occurs")
     return (
         (coord["x1"] + offset[0])**2 
         + (coord["x2"] + offset[1])**2 
@@ -21,17 +24,17 @@ if not os.path.exists(data_path):
 
 class TestOpt():
     # ##############################################################################
-    def create_opt(self):
+    def create_opt(self, error_prob=0.0):
         opt = Optimization(
             fixed_variables=fixed_params,
             free_variable_ranges=bounds,
             target_func=target_func,
-            target_kwargs={"offset": offset},
+            target_kwargs={"offset": offset, "error_prob": error_prob},
         )
         return opt
 
     def test_opt(self):
-        opt = self.create_opt()
+        opt = self.create_opt(error_prob=0.0)
         traj = opt.run(
             file_name = f"{data_path}/traj.csv", 
             fixed_para_file_name = f"{data_path}/traj_fixed.csv"
@@ -54,20 +57,29 @@ class TestOpt():
     # ##############################################################################
     def create_multi_opt(self):
         multi_opt = MultiOpt(
-            self.create_opt(),
+            self.create_opt(error_prob=0.03),
         )
         return multi_opt
     
-    def test_multi_opt(self):
-        multi_path = f"{data_path}/multi_traj/"
+    def create_and_clear_folder(self, multi_path):
+        # create a folder to store the multi_traj
         if not os.path.exists(multi_path):
             os.mkdir(multi_path)
+        # clear the folder
+        for file_name in os.listdir(multi_path):
+            os.remove(f"{multi_path}/{file_name}")
+    
+    def run_multi_opt(self):
+        multi_path = f"{data_path}/multi_traj/"
+        self.create_and_clear_folder(multi_path)
 
         multi_opt = self.create_multi_opt()
         multi_traj = multi_opt.run(
-            3,
+            20,
             save_path=multi_path,
+            cpu_num=4
         )
+        print(multi_traj.length)
 
         para = multi_traj.best_traj().final_full_para
         assert np.abs(para["x1"] + offset[0]) < 1e-5
@@ -76,6 +88,16 @@ class TestOpt():
         loaded_multi_traj = MultiTraj.from_folder(multi_path)
         loaded_para = loaded_multi_traj.best_traj().final_full_para
 
-        assert loaded_para["x1"] == para["x1"]
-        assert loaded_para["x2"] == para["x2"]
-        assert loaded_para["x3"] == para["x3"]
+        assert loaded_multi_traj.length == multi_traj.length
+        assert np.abs(para["x1"] - loaded_para["x1"]) < 1e-15
+        assert np.abs(para["x2"] - loaded_para["x2"]) < 1e-15
+        assert np.abs(para["x3"] - loaded_para["x3"]) < 1e-15
+    
+    def test_multi_opt(self):
+        for _ in range(3):
+            self.run_multi_opt()
+
+if __name__ == "__main__":
+    test_opt = TestOpt()
+    # test_opt.test_opt()
+    test_opt.test_multi_opt()
