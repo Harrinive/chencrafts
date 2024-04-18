4a6eef632ed8772b5898edf60cf339de4b348f8b
Danyang Chen <dychen1999@foxmail.com>
2024-04-18 00:16:23 -0500

    build requirements

--- setup.py
+++ setup.py
@@ -1,19 +1,11 @@
-import setuptools
 import os
+import sys
 
-VERSION = 0.3
-# when changed, remember to change chencrafts/__init__.py
-PACKAGES = [
-    'chencrafts',
-    'chencrafts.toolbox',
-    'chencrafts.bsqubits',
-    'chencrafts.bsqubits.QEC_graph',
-    'chencrafts.cqed',
-    'chencrafts.cqed.custom_sweeps',
-    'chencrafts.specfit',
-    'chencrafts.projects',
-]
+from setuptools import setup, find_packages
 
+
+VERSION = 1.0
+PACKAGES = find_packages()
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 with open(os.path.join(CURRENT_DIR, "requirements.txt")) as requirements:
     INSTALL_REQUIRES = requirements.read().splitlines()
@@ -22,7 +14,37 @@ EXTRA_REQUIRES = {
     "bsqubits": ["networkx"]
 }
 
-setuptools.setup(name='chencrafts', 
+
+CLASSIFIERS = """\
+Development Status :: 4 - Beta
+Intended Audience :: Science/Research
+License :: OSI Approved :: BSD License
+Programming Language :: Python
+Programming Language :: Python :: 3
+Topic :: Scientific/Engineering
+Operating System :: MacOS
+Operating System :: POSIX
+Operating System :: Unix
+Operating System :: Microsoft :: Windows
+"""
+CLASSIFIERS = [_f for _f in CLASSIFIERS.split("\n") if _f]
+PLATFORMS = ["Linux", "Mac OSX", "Unix", "Windows"]
+KEYWORDS = "personal toolbox, superconducting qubits, quantum mechanics"
+
+
+EXTRA_KWARGS = {}
+
+
+# write a version.py file
+version_path = os.path.join(CURRENT_DIR, 'chencrafts', 'version.py')
+with open(version_path, "w") as versionfile:
+    versionfile.write(
+        f"# THIS FILE IS GENERATED FROM chencrafts SETUP.PY\n"
+        f"version = '{VERSION}'"
+    )
+
+
+setup(name='chencrafts', 
     version=VERSION,
     description='Danyang Chen\'s personal toolbox',
     url='https://github.com/Harrinive/chencrafts',
@@ -34,17 +56,12 @@ setuptools.setup(name='chencrafts',
     install_requires=INSTALL_REQUIRES,
     extras_require=EXTRA_REQUIRES,
     python_requires='>=3.10',
+    classifiers=CLASSIFIERS,
+    platforms=PLATFORMS,
+    keywords=KEYWORDS,
+    **EXTRA_KWARGS
 )
 
-def write_version_py(filename="chencrafts/version.py"):
-    if os.path.exists(filename):
-        os.remove(filename)
-    versionfile = open(filename, "w")
-    try:
-        versionfile.write(
-            f"# THIS FILE IS GENERATED FROM chencrafts SETUP.PY\n"
-            "version = '{VERSION}'"
-        )
-    finally:
-        versionfile.close()
-write_version_py()
+
+
+
